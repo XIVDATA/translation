d614c07e863c58178df536ee8baf8e24d32e89c9
Syntafin <syntafin@antribute.eu>
2020-05-03 04:05:05 +0200

    Forgot the String Name

--- fr/footer.php
+++ fr/footer.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'Créé avec <i class="mdi mdi-heart text-danger"></i> par <a href="https://syntafin.dev" target="_blank">Syntafin</a>. Pour une Eorzéa meilleure.'
+    'copyright_xivdata' => 'Créé avec <i class="mdi mdi-heart text-danger"></i> par <a href="https://syntafin.dev" target="_blank">Syntafin</a>. Pour une Eorzéa meilleure.'
 ];
