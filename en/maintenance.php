5d50b1f7526b13b8794fdc43d0bb19d12ad6473e
Syntafin <syntafin@antribute.eu>
2020-06-15 01:10:45 +0200

    Create maintenance.php

---
+++ en/maintenance.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'undermaintenance' => 'Page is in Maintenance Mode',
+    'faq1' => 'What does this mean?',
+    'faq2' => 'How long will it take?',
+    'faq3' => 'Need Support?',
+    'faq1txt' => 'The Page is actual under Maintenance Mode, when we are done with it, the page will be back online, just look in a few Minutes again or check Discord/Twitter for furter updates!',
+    'faq2txt' => 'In the most cases, this is just when we apply a new update, so it just take a few minutes. If there is a longer Outtime, we will post something in Discord/Twitter to inform you! Please dont ask how long it will take there, it is done when it is done.',
+    'faq3txt' => 'You have a critical question about the Page/Service? Dont hesistate to ask on our Discord! Please dont ask things about the game just because the Page is in Maintenance (you can ask on the Discord, but please not in the Support-Channel!).'
+];
